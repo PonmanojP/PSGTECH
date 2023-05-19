908a4340f1a7dccab61258f9c9719fa3bc663705
Manoj <manojbilla2005@gmail.com>
2023-05-19 21:32:34 +0530

    change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,77 @@
 $(document).ready(function(){
-  $('.demo').slick({
-  	accessibility: true,
-  	arrows:true,
-
-  });
-});
+    $('.center').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 4,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            autoplay:true,
+            autoplaySpeed:3000,
+            centerPadding: '40px',
+            infinite:true,
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            autoplay:true,
+            autoplaySpeed:3000,
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1,
+            mobileFirst:true,
+            infinite:true,
+            dots:true
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            autoplay:true,
+            autoplaySpeed:3000,
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 2,
+            mobileFirst:true,
+            infinite:true,
+            dots:true
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            autoplay:true,
+            autoplaySpeed:3000,
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 2,
+            mobileFirst:true,
+            infinite:true,
+            dots:true
+          }
+        },
+        {
+          breakpoint:1600,
+          settings: {
+            autoplay:true,
+            autoplaySpeed:2000,
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3,
+            mobileFirst:true,
+            infinite:true,
+            dots:true
+          }
+        }
+      ]
+    });
+    });
